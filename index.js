e66c29fe752ba0e6bfe6bf591d286a6848d1d8ae
Sandeep Shah <sandeep2016shah@gmail.com>
2024-02-25 22:16:58 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import Freecurrencyapi from '@everapi/freecurrencyapi-js';
+
+const freecurrencyapi = new Freecurrencyapi('fca_live_TmPgAd52KfJdlctwU4wOo1DeCOUoX3ofRux04QPw');
+
+
+export async function convertCurrency(fromCurrency, toCurrency, units){
+    const response = await freecurrencyapi.latest({
+        base_currency: 'USD',
+        currencies: 'EUR'
+    });
+
+    const multiplier = response?.data?.[toCurrency] ?? 0;
+
+    return multiplier * units;
+}
